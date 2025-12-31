af1f8820d8b66d79815885e7697a85bd9361d98a
Mah M UD <mahmudx077@gmail.com>
2025-12-31 14:41:00 +0600

    Update anime.js

--- anime.js
+++ anime.js
@@ -47,7 +47,7 @@ module.exports = {
       }, 5000);
 
       const apiUrl = await mahmud();
-      const res = await axios.get(`${apiUrl}/api/album/videos/anime?userID=${event.senderID}`);
+      const res = await axios.get(`${apiUrl}/api/album/mahmud/videos/anime?userID=${event.senderID}`);
       if (!res.data.success || !res.data.videos.length)
         return api.sendMessage("❌ | No videos found.", event.threadID, event.messageID);
 
