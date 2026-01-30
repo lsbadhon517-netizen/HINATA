888ac2c626d8638fb495d4e5eaa38042058a97dd
Mah M UD <mahmudx077@gmail.com>
2026-01-30 07:33:22 +0600

    Create imgbb.js

---
+++ imgbb.js
@@ -0,0 +1,58 @@
+const axios = require("axios");
+
+const getBase = async () => {
+    const res = await axios.get("https://raw.githubusercontent.com/mahmudx7/HINATA/main/baseApiUrl.json");
+    return res.data.mahmud;
+};
+
+/**
+* @author MahMUD
+* @author: do not delete it
+*/
+
+module.exports = {
+    config: {
+        name: "imgbb",
+        version: "1.7",
+        author: "MahMUD",
+        countDown: 10,
+        role: 0,
+        category: "tools",
+        guide: { en: "{pn} [reply to media]" }
+    },
+
+    onStart: async function ({ api, event, message }) {
+          const obfuscatedAuthor = String.fromCharCode(77, 97, 104, 77, 85, 68); 
+          if (module.exports.config.author !== obfuscatedAuthor) {
+          return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
+       }
+        
+        if (event.type !== "message_reply" || !event.messageReply.attachments.length) {
+           return message.reply("🐤 | Please reply to a media file image video mp4.");
+        }
+
+        try {  
+            api.setMessageReaction("⌛", event.messageID, () => {}, true);  
+
+            const attachmentUrl = encodeURIComponent(event.messageReply.attachments[0].url);
+            const baseUrl = await getBase();
+            const apiUrl = `${baseUrl.replace(/\/$/, "")}/api/imgbb?url=${attachmentUrl}`;  
+            const response = await axios.get(apiUrl, { timeout: 100000 });
+            
+            if (response.data.status && response.data.link) {
+                message.reply({  
+                    body: "•𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐒𝐮𝐜𝐜𝐞𝐬𝐬 ✅\n•𝐔𝐑𝐋: " + response.data.link
+                }, () => {  
+                    api.setMessageReaction("✅", event.messageID, () => {}, true);  
+                });
+            } else {
+                throw new Error("API status false");
+            }
+
+        } catch (e) {  
+            console.error(e);
+            api.setMessageReaction("❌", event.messageID, () => {}, true);
+            message.reply("🥺 error, contact MahMUD");  
+        }
+    }
+};
