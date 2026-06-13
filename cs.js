34196bca41e39a9fa78cd8c25bf9c8f1f3d65242
MahMUD <mahmudx077@gmail.com>
2026-06-13 20:59:33 +0600

    Update cs.js

--- cs.js
+++ cs.js
@@ -9,7 +9,7 @@ module.exports = {
         config: {
                 name: "cmdstore",
                 aliases: ["cmds", "cs"],
-                version: "1.7",
+                version: "2.7",
                 author: "MahMUD",
                 countDown: 5,
                 role: 0,
